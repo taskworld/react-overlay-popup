9f70d38c12c49cfb0c8069ec08f813371805c936
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-03-29 14:37:42 +0700

    Add test!

--- nwb.config.js
+++ nwb.config.js
@@ -1,21 +1,22 @@
-module.exports = {
+module.exports = nwb => ({
   // Let nwb know this is a React component module when generic build commands
   // are used.
   type: 'react-component',
 
-  // Should nwb create a UMD build for this module?
-  umd: false,
-  // The name of the global variable the UMD build of this module will export
-  global: '',
-  // Mapping from the npm package names of this module's peerDependencies to the
-  // global variables they're expected to be available as for use by the UMD
-  // build.
-  externals: {
-    'react': 'React'
+  build: {
+    umd: false,
+    global: '',
+    externals: {
+      'react': 'React'
+    },
+    jsNext: true
   },
 
-  // Should nwb create a build with untranspiled ES6 modules for tree-shaking
-  // module bundlers? If you change your mind later, add or remove this line in
-  // package.json: "jsnext:main": "es6/index.js"
-  jsNext: true
-}
+  webpack: {
+    extra: {
+      plugins: [
+        new nwb.webpack.IgnorePlugin(/react\/lib\/(?:ExecutionEnvironment|ReactContext)/)
+      ]
+    }
+  }
+})
